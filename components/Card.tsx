07b1381a19f4d132463341571d86b09a338aafae
lancedalanon <lanceorville5@gmail.com>
2024-10-03 19:10:05 +0800

    Update card to accept props

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,17 @@
 
 import React from 'react';
 
-interface CardProps {
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
 }
 
-const Card: React.FC<CardProps> = ({ children, className }) => {
+const Card: React.FC<CardProps> = ({ children, className, ...props }) => {
   return (
-    <div className={`p-4 shadow-md rounded-lg overflow-hidden ${className}`}>
+    <div 
+      className={`p-4 shadow-md rounded-lg overflow-hidden ${className}`}
+      {...props}
+    >
       {children}
     </div>
   );
--- components/home/SkillsSection.tsx
+++ components/home/SkillsSection.tsx
@@ -37,9 +37,17 @@ const SkillsSection: React.FC = () => {
 
 const SkillCard: React.FC<{ skill: Skill }> = ({ skill }) => {  
   const IconComponent = skillIcons[skill.skill_icon];  
+
+  // Function to handle card click
+  const handleClick = () => {
+    console.log(skill.id); // Log the skill id to the console
+  };
   
   return (
-    <Card className="max-w-sm w-full bg-custom-700 transition-transform transform hover:scale-105 hover:shadow-lg cursor-pointer">
+    <Card 
+     className="max-w-sm w-full bg-custom-700 transition-transform transform hover:scale-105 hover:shadow-lg cursor-pointer"
+     onClick={handleClick}
+    >
       <CardHeader>
         <div className="text-white text-7xl flex flex-col items-center justify-center text-center mb-2">
           {IconComponent && <IconComponent />}
