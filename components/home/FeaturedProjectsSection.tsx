e3158858c28ad998ce0f30e86cd50e6c3bcac8d0
lancedalanon <lanceorville5@gmail.com>
2024-10-05 15:54:06 +0800

    Change More Details icon to be magnifying glass instead

--- components/home/FeaturedProjectsSection.tsx
+++ components/home/FeaturedProjectsSection.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import projects from '@/constants/projects.json';
 
@@ -15,7 +16,7 @@ const FeaturedProjectsSection: React.FC = () => {
                 {/* Iterate over the featured projects */}
                 {projects.filter(project => project.is_featured === 1).map((project) => (
                     <React.Fragment key={project.id}>
-                        <div className="relative group hover:cursor-pointer">
+                        <div className="relative group">
                             <Image
                                 alt={project.project_name}
                                 src={project.project_image}
@@ -25,11 +26,11 @@ const FeaturedProjectsSection: React.FC = () => {
                             />
                             <div className="absolute inset-0 bg-custom-600 bg-opacity-0 group-hover:bg-opacity-90 rounded-3xl flex flex-col justify-between items-start transition-all duration-500 ease-in-out p-6">
                                 {/* Title */}
-                                <span className="text-white text-4xl font-bold opacity-0 group-hover:opacity-100 transform -translate-y-4 group-hover:translate-y-0 transition-all duration-500 ease-in-out md:mt-6">
+                                <span className="text-white text-2xl md:text-4xl font-bold opacity-0 group-hover:opacity-100 transform -translate-y-4 group-hover:translate-y-0 transition-all duration-500 ease-in-out md:mt-6">
                                     <p>{project.project_name}</p>
                                 </span>
                                 {/* Description */}
-                                <span className="text-white text-lg opacity-0 group-hover:opacity-100 transform translate-y-4 group-hover:translate-y-0 transition-all duration-500 ease-in-out mt-6 hidden md:block">
+                                <span className="text-white text-sm md:text-lg hidden md:block opacity-0 group-hover:opacity-100 transform translate-y-4 group-hover:translate-y-0 transition-all duration-500 ease-in-out mt-6">
                                     <p>{project.short_description}</p>
                                 </span>
                                 {/* Button */}
@@ -40,7 +41,7 @@ const FeaturedProjectsSection: React.FC = () => {
                                         passHref
                                         className="bg-accent text-white text-lg md:text-2xl rounded-3xl hover:bg-accent-light active:bg-accent-dark px-5 py-3 md:px-10 md:py-5 font-bold flex items-center"
                                     >
-                                        <FaExternalLinkAlt className="mr-2" />
+                                        <FaMagnifyingGlass className="mr-2" />
                                         More Details
                                     </Link>
                                 </span>
