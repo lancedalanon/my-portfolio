4135fb962c3fb8310c53bccd4158c539fd8e71cb
lancedalanon <lanceorville5@gmail.com>
2024-10-08 22:18:16 +0800

    Update experience.json to remove description

--- types/experienceTypes.ts
+++ types/experienceTypes.ts
@@ -1,3 +1,5 @@
+import { LanguageFrameworkLibrary, Technology, Skill } from "@/types/projectTypes";
+
 export default interface Experience {
     id: number;                     
     image: string;                   
@@ -8,5 +10,9 @@ export default interface Experience {
     month_started: string;           
     year_started: string;            
     month_ended: string | null;      
-    year_ended: string | null;      
+    year_ended: string | null;
+    experiences: string[] | null;     
+    language_framework_libraries: LanguageFrameworkLibrary[];
+    technologies: Technology[];
+    skills: Skill[]; 
 }
