3c3e39dd6942cbf778b056457680613b441c502c
lancedalanon <lanceorville5@gmail.com>
2024-10-10 08:45:08 +0800

    Update next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+            source: '/(.*)',
+            headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: 'index, follow',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
